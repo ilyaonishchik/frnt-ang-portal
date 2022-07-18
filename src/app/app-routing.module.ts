d2eda1a9fc92423d235ed0aa2c0086549d46c57a
AnViSe <anvise@grodno.belpost.by>
2022-07-18 23:10:14 +0300

    Add: Append module auth

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import {NgModule} from '@angular/core'
 import {RouterModule, Routes} from '@angular/router'
+import {Page404Component} from './components/page404/page404.component'
 
-const routes: Routes = []
+const routes: Routes = [
+  {
+    path: 'auth',
+    loadChildren: () =>
+      import('./modules/auth/auth.module').then((m) => m.AuthModule),
+  },
+  {path: '**', component: Page404Component},
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
