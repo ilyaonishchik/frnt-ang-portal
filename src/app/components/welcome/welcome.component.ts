5ae690b1f7a7327ddc51f06215c9c9c50ca524df
AnViSe <anvise@grodno.belpost.by>
2022-07-19 17:18:46 +0300

    Upd: Change component Welcome

--- src/app/components/welcome/welcome.component.ts
+++ src/app/components/welcome/welcome.component.ts
@@ -1,15 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core'
+import {WelcomeService} from './service/welcome.service'
+import {FaIconLibrary} from '@fortawesome/angular-fontawesome'
+import {
+  faRightFromBracket,
+  faRightToBracket,
+  faUserPlus,
+} from '@fortawesome/free-solid-svg-icons'
 
 @Component({
   selector: 'app-welcome',
   templateUrl: './welcome.component.html',
-  styleUrls: ['./welcome.component.scss']
+  styleUrls: ['./welcome.component.scss'],
 })
 export class WelcomeComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private welcomeService: WelcomeService,
+    libraryIcons: FaIconLibrary
+  ) {
+    libraryIcons.addIcons(faRightToBracket, faUserPlus, faRightFromBracket)
   }
 
+  ngOnInit(): void {}
+
+  isSignIn() {
+    return false
+  }
+
+  logout() {}
+
+  isProduction() {
+    return this.welcomeService.isProduction
+  }
+
+  getProjectTitle() {
+    return this.welcomeService.projectTitle
+  }
+
+  getLinks() {
+    return this.welcomeService.getLinks()
+  }
+
+  showLink(link: string) {
+    window.open(link, '_blank')
+  }
 }
