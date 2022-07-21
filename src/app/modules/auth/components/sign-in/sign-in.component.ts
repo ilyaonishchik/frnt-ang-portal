d5e852ef1124d2cac4cbd4c3494079587fc7c09c
AnViSe <anvise@grodno.belpost.by>
2022-07-21 12:09:21 +0300

    Ref: Sign-in component

--- src/app/modules/auth/components/sign-in/sign-in.component.ts
+++ src/app/modules/auth/components/sign-in/sign-in.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core'
+import {AppService} from '../../../../services/app.service'
 
 @Component({
   selector: 'app-sign-in',
   templateUrl: './sign-in.component.html',
-  styleUrls: ['./sign-in.component.scss']
+  styleUrls: ['./sign-in.component.scss'],
 })
 export class SignInComponent implements OnInit {
+  constructor(private appService: AppService) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  getProjectTitle() {
+    return this.appService.projectTitle
   }
-
 }
