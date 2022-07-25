d9017c5d8ecde2487266f728a48ec8d7a9c20e0b
AnViSe <anvise@grodno.belpost.by>
2022-07-25 10:44:09 +0300

    Add: Admin module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,20 @@ import {NgModule} from '@angular/core'
 import {RouterModule, Routes} from '@angular/router'
 import {Page404Component} from './components/page404/page404.component'
 import {WelcomeComponent} from './components/welcome/welcome.component'
+import {AuthGuard} from './guards/auth.guard'
 
 const routes: Routes = [
   {
     path: 'auth',
     loadChildren: () =>
       import('./modules/auth/auth.module').then((m) => m.AuthModule),
+  },
+  {
+    path: 'admin',
+    canActivate: [AuthGuard],
+    canDeactivate: [AuthGuard],
+    loadChildren: () =>
+      import('./modules/admin/admin.module').then((m) => m.AdminModule),
   },
   {path: 'welcome', component: WelcomeComponent},
   {path: '', redirectTo: 'welcome', pathMatch: 'full'},
