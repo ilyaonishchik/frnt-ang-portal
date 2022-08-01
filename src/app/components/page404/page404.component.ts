f3d683550af26bee93f8d4ec2966646f7c53b980
AnViSe <anvise@grodno.belpost.by>
2022-08-01 23:23:28 +0300

    Ref: Page404 component

--- src/app/components/page404/page404.component.ts
+++ src/app/components/page404/page404.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core'
 import {FaIconLibrary} from '@fortawesome/angular-fontawesome'
 import {faQuestionCircle} from '@fortawesome/free-regular-svg-icons'
+import {Location} from '@angular/common'
 
 @Component({
   selector: 'app-page404',
@@ -8,9 +9,13 @@ import {faQuestionCircle} from '@fortawesome/free-regular-svg-icons'
   styleUrls: ['./page404.component.scss'],
 })
 export class Page404Component implements OnInit {
-  constructor(library: FaIconLibrary) {
+  constructor(library: FaIconLibrary, private location: Location) {
     library.addIcons(faQuestionCircle)
   }
 
   ngOnInit(): void {}
+
+  goBack(): void {
+    this.location.back()
+  }
 }
