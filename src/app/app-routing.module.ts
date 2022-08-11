2cfa5149d56684e93f122f2bade348a596e64102
AnViSe <anvise@grodno.belpost.by>
2022-08-11 12:14:20 +0300

    Ref: App routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,22 +1,28 @@
 import {NgModule} from '@angular/core'
 import {RouterModule, Routes} from '@angular/router'
 import {Page404Component} from './components/page404/page404.component'
-import {WelcomeComponent} from './components/welcome/welcome.component'
 import {AuthGuard} from './guards/auth.guard'
+import {WelcomeComponent} from './components/welcome/welcome.component'
 
 const routes: Routes = [
+  {
+    path: 'default',
+    canActivate: [AuthGuard],
+    loadChildren: () =>
+      import('./modules/default/default.module').then((m) => m.DefaultModule),
+  },
   {
     path: 'auth',
     loadChildren: () =>
       import('./modules/auth/auth.module').then((m) => m.AuthModule),
   },
-  {
-    path: 'admin',
-    canActivate: [AuthGuard],
-    canDeactivate: [AuthGuard],
-    loadChildren: () =>
-      import('./modules/admin/admin.module').then((m) => m.AdminModule),
-  },
+  // {
+  //   path: 'admin',
+  //   canActivate: [AuthGuard],
+  //   canDeactivate: [AuthGuard],
+  //   loadChildren: () =>
+  //     import('./modules/admin/admin.module').then((m) => m.AdminModule),
+  // },
   {
     path: 'sortirovka',
     loadChildren: () =>
@@ -24,9 +30,11 @@ const routes: Routes = [
         (m) => m.SortirovkaModule
       ),
   },
-  {path: 'welcome', component: WelcomeComponent},
-  // {path: '', redirectTo: 'welcome', pathMatch: 'full'},
-  {path: '', redirectTo: 'sortirovka', pathMatch: 'full'},
+  {
+    path: 'welcome',
+    component: WelcomeComponent,
+  },
+  {path: '', redirectTo: 'default', pathMatch: 'full'},
   {path: '**', component: Page404Component},
 ]
 
