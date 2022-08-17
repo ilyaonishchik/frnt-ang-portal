086cdb5b74a674cc625cacb3ceaf4d9d833c46e6
AnViSe <anvise@grodno.belpost.by>
2022-08-17 10:23:18 +0300

    Ref: Datepicker component

--- src/app/modules/shared/components/datepicker/datepicker.component.ts
+++ src/app/modules/shared/components/datepicker/datepicker.component.ts
@@ -1,40 +1,89 @@
-import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core'
-import {NgbDate} from '@ng-bootstrap/ng-bootstrap'
+import {
+  Component,
+  EventEmitter,
+  Injectable,
+  Input,
+  OnInit,
+  Output,
+} from '@angular/core'
+import {
+  NgbDateAdapter,
+  NgbDateParserFormatter,
+  NgbDateStruct,
+} from '@ng-bootstrap/ng-bootstrap'
+
+@Injectable()
+export class CustomAdapter extends NgbDateAdapter<string> {
+  readonly DELIMITER = '.'
+
+  override fromModel(value: string | null): NgbDateStruct | null {
+    if (value) {
+      const date = value.split(this.DELIMITER)
+      return {
+        day: parseInt(date[0], 10),
+        month: parseInt(date[1], 10),
+        year: parseInt(date[2], 10),
+      }
+    }
+    return null
+  }
+
+  override toModel(date: NgbDateStruct | null): string | null {
+    return date
+      ? date.day.toString().padStart(2, '0') +
+          this.DELIMITER +
+          date.month.toString().padStart(2, '0') +
+          this.DELIMITER +
+          date.year
+      : null
+  }
+}
+
+@Injectable()
+export class CustomDateParseFormatter extends NgbDateParserFormatter {
+  readonly DELIMITER = '.'
+
+  override parse(value: string): NgbDateStruct | null {
+    if (value) {
+      const date = value.split(this.DELIMITER)
+      return {
+        day: parseInt(date[0], 10),
+        month: parseInt(date[1], 10),
+        year: parseInt(date[2], 10),
+      }
+    }
+    return null
+  }
+
+  override format(date: NgbDateStruct | null): string {
+    return date
+      ? date.day.toString().padStart(2, '0') +
+          this.DELIMITER +
+          date.month.toString().padStart(2, '0') +
+          this.DELIMITER +
+          date.year
+      : ''
+  }
+}
 
 @Component({
   selector: 'app-datepicker',
   templateUrl: './datepicker.component.html',
   styleUrls: ['./datepicker.component.scss'],
+  providers: [
+    {provide: NgbDateAdapter, useClass: CustomAdapter},
+    {provide: NgbDateParserFormatter, useClass: CustomDateParseFormatter},
+  ],
 })
 export class DatepickerComponent implements OnInit {
-  valueDate!: NgbDate
-  // @Input() dateValue: string = ''
-  @Input()
-  set dateValue(dateValue: string) {
-    const dateParts = dateValue.split('.')
-    this.valueDate = new NgbDate(
-      parseInt(dateParts[2]),
-      parseInt(dateParts[1]),
-      parseInt(dateParts[0])
-    )
-  }
-
+  @Input() valueDate: string | undefined
   @Output() changed = new EventEmitter<string>()
 
   constructor() {}
 
   ngOnInit(): void {}
 
-  get dateValue(): string {
-    return `${this.valueDate.day
-      .toString()
-      .padStart(2, '0')}.${this.valueDate.month
-      .toString()
-      .padStart(2, '0')}.${this.valueDate.year.toString()}`
-  }
-
-  change(value: string) {
-    console.log(`Picker: ${value}`)
-    this.changed.emit(value)
+  change() {
+    this.changed.emit(this.valueDate)
   }
 }
