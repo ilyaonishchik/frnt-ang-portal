af51b4cf3537f9a95578b3f0cc871b4c1a0196bb
AnViSe <anvise@grodno.belpost.by>
2022-08-17 10:14:46 +0300

    Ref: Register locale

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,18 @@ import {BrowserModule} from '@angular/platform-browser'
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap'
 import {FontAwesomeModule} from '@fortawesome/angular-fontawesome'
 
+import {SharedModule} from './modules/shared/shared.module'
 import {AppRoutingModule} from './app-routing.module'
+
 import {AppComponent} from './app.component'
 import {Page404Component} from './components/page404/page404.component'
-import {SharedModule} from './modules/shared/shared.module'
 import {ToastComponent} from './components/toast/toast.component'
 import {WelcomeComponent} from './components/welcome/welcome.component'
+
+import {registerLocaleData} from '@angular/common'
+import localeRu from '@angular/common/locales/ru'
+
+registerLocaleData(localeRu)
 
 @NgModule({
   declarations: [
