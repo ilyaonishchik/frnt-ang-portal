47417e0fc40f56496a812cf256a6aee9797ce2b8
AnViSe <anvise@grodno.belpost.by>
2022-09-08 17:32:09 +0300

    Upd: Toast messages 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,9 @@
 import {NgModule} from '@angular/core'
 import {BrowserModule} from '@angular/platform-browser'
 
+import {ToastModule} from 'primeng/toast'
+import {MessageService} from 'primeng/api'
+
 import {AppRoutingModule} from './app-routing.module'
 import {AppLayoutModule} from './layout/app.layout.module'
 
@@ -8,8 +11,8 @@ import {AppComponent} from './app.component'
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, AppLayoutModule],
-  providers: [],
+  imports: [BrowserModule, AppRoutingModule, AppLayoutModule, ToastModule],
+  providers: [MessageService],
   bootstrap: [AppComponent],
   exports: [],
 })
