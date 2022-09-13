4eeeb35ee99eaa8f4c837ca1498b7a7f4cee362d
AnViSe <anvise@grodno.belpost.by>
2022-09-13 17:06:41 +0300

    Upd: Admin routes

--- src/app/modules/admin/admin-routing.module.ts
+++ src/app/modules/admin/admin-routing.module.ts
@@ -2,10 +2,13 @@ import {NgModule} from '@angular/core'
 import {RouterModule, Routes} from '@angular/router'
 
 import {MainComponent} from './components/main/main.component'
+import {DashboardComponent} from './components/dashboard/dashboard.component'
+import {UsersComponent} from './components/users/users.component'
 
 const routes: Routes = [
   {path: '', component: MainComponent},
-  // {path: 'main', component: MainComponent},
+  {path: 'dashboard', component: DashboardComponent},
+  {path: 'users', component: UsersComponent},
   // {path: '**', redirectTo: 'main', pathMatch: 'full'},
 ]
 
--- src/app/modules/admin/admin.module.ts
+++ src/app/modules/admin/admin.module.ts
@@ -4,10 +4,21 @@ import {CommonModule} from '@angular/common'
 import {AdminRoutingModule} from './admin-routing.module'
 
 import {MainComponent} from './components/main/main.component'
+import {DashboardComponent} from './components/dashboard/dashboard.component'
+import {UsersComponent} from './components/users/users.component'
+import {TableModule} from 'primeng/table'
+import {ButtonModule} from 'primeng/button'
+import {InputTextModule} from 'primeng/inputtext'
 
 @NgModule({
-  declarations: [MainComponent],
-  imports: [CommonModule, AdminRoutingModule],
+  declarations: [MainComponent, DashboardComponent, UsersComponent],
+  imports: [
+    CommonModule,
+    AdminRoutingModule,
+    TableModule,
+    ButtonModule,
+    InputTextModule,
+  ],
   exports: [],
 })
 export class AdminModule {}
