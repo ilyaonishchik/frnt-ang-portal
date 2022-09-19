0a933bb03337cdf8015d9e5a3089ec094b0c8987
AnViSe <anvise@grodno.belpost.by>
2022-09-19 17:05:17 +0300

    Upd: Desc to Comment

--- src/app/modules/admin/components/users/user.ts
+++ src/app/modules/admin/components/users/user.ts
@@ -2,7 +2,7 @@ export interface IUser {
   id: number
   username: string
   email: string
-  desc?: string
+  comment?: string
   avatar?: string
   verify?: string
   last_login?: string
--- src/app/types/user.ts
+++ src/app/types/user.ts
@@ -2,7 +2,7 @@ export interface IUser {
   id: number
   username: string
   email?: string
-  desc?: string
+  comment?: string
   avatar?: string
 }
 
