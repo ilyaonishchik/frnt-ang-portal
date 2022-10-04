0a75b69e4a2177d3eee09eb4e597ee0cfd641206
AnViSe <anvise@grodno.belpost.by>
2022-10-04 17:15:10 +0300

    Ref: Clear code

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,10 +6,8 @@ export const environment = {
   production: false,
   title: 'Бизнес-портал',
   version: '0.0.6',
-  // urlApiAuth: 'http://172.16.190.221:8001/api/v1/auth/',
   urlApiAuth: '/api/v1/auth/',
   urlApiPdp: '/api/v1/sorting/',
-  // urlApiAuth: 'http://localhost:8000/api/v1/auth/',
 }
 
 /*
