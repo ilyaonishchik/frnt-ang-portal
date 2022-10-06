d77c5886f0098db807bb94cbcd90293dede480b6
AnViSe <anvise@grodno.belpost.by>
2022-10-06 12:33:09 +0300

    Ref: Clear code

--- src/app/modules/podpiska/podpiska.module.ts
+++ src/app/modules/podpiska/podpiska.module.ts
@@ -1,14 +1,15 @@
 import {NgModule} from '@angular/core'
 import {CommonModule} from '@angular/common'
+import {FormsModule} from '@angular/forms'
+
+import {CardModule} from 'primeng/card'
+import {CalendarModule} from 'primeng/calendar'
+import {DropdownModule} from 'primeng/dropdown'
+import {InputTextModule} from 'primeng/inputtext'
 
 import {PodpiskaRoutingModule} from './podpiska-routing.module'
 import {MainComponent} from './components/main/main.component'
 import {SortirovkaComponent} from './components/sortirovka/sortirovka.component'
-import {CardModule} from 'primeng/card'
-import {CalendarModule} from 'primeng/calendar'
-import {DropdownModule} from 'primeng/dropdown'
-import {FormsModule} from '@angular/forms'
-import {InputTextModule} from 'primeng/inputtext'
 
 @NgModule({
   declarations: [MainComponent, SortirovkaComponent],
