a6e3d5a17dc26d16e5051e8e290a75184b6b2ee4
AnViSe <anvise@grodno.belpost.by>
2022-10-14 17:35:02 +0300

    Add: Append after delete effect

--- src/app/modules/admin/sections/auth/permissions/store/effects/delete.effect.ts
+++ src/app/modules/admin/sections/auth/permissions/store/effects/delete.effect.ts
@@ -1,6 +1,7 @@
 import {Injectable} from '@angular/core'
-import {catchError, map, of, switchMap} from 'rxjs'
+import {catchError, map, of, switchMap, tap} from 'rxjs'
 import {Actions, createEffect, ofType} from '@ngrx/effects'
+import {Store} from '@ngrx/store'
 
 import {PermissionsService} from '../../services/permissions.service'
 import {
@@ -10,11 +11,13 @@ import {
 } from '../actions/delete.action'
 import {IDeleteResponse} from 'src/app/shared/interfaces/delete-response.interface'
 import {IActionErrorResponse} from 'src/app/shared/interfaces/action-error-response.interface'
+import {getPermissionsAction} from '../actions/permissions.action'
 
 @Injectable()
 export class DeletePermissionEffect {
   constructor(
     private actions$: Actions,
+    private store: Store,
     private permissionsService: PermissionsService
   ) {}
 
@@ -32,5 +35,16 @@ export class DeletePermissionEffect {
         )
       })
     )
+  )
+
+  afterDeletePermission$ = createEffect(
+    () =>
+      this.actions$.pipe(
+        ofType(deletePermissionSuccessAction),
+        tap(() => {
+          this.store.dispatch(getPermissionsAction({event: null}))
+        })
+      ),
+    {dispatch: false}
   )
 }
