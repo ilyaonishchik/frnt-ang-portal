2af12b39a8bdd0da1bf6bc5fe79f4e5c76e758a7
AnViSe <anvise@grodno.belpost.by>
2022-10-18 15:18:17 +0300

    Add: Dynamic components files

---
+++ src/app/shared/classes/form.class.ts
@@ -0,0 +1,5 @@
+import {Type} from '@angular/core'
+
+export class FormValuesComponent {
+  constructor(public component: Type<any>, public initialValues: any) {}
+}
---
+++ src/app/shared/directives/form.directive.ts
@@ -0,0 +1,8 @@
+import {Directive, ViewContainerRef} from '@angular/core'
+
+@Directive({
+  selector: '[appForm]',
+})
+export class FormDirective {
+  constructor(public viewContainerRef: ViewContainerRef) {}
+}
