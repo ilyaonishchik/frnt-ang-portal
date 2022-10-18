1eff3aae43dd672fb482106ae722726ba99fbc23
AnViSe <anvise@grodno.belpost.by>
2022-10-18 21:52:30 +0300

    Add: Permission state

--- src/app/shared/interfaces/app-state.interface.ts
+++ src/app/shared/interfaces/app-state.interface.ts
@@ -1,5 +1,6 @@
 import {IAuthState} from '../../modules/auth/interfaces/auth-state.interface'
 import {IPermissionsState} from '../../modules/admin/sections/auth/permissions/interfaces/permissions-state.interface'
+import {IPermissionState} from '../../modules/admin/components/permission/interfaces/permission-state.interface'
 import {IRolesState} from '../../modules/admin/sections/auth/roles/interfaces/roles-state.interface'
 import {IUsersState} from '../../modules/admin/sections/auth/users/interfaces/users-state.interface'
 import {IUserState} from '../../modules/admin/components/user/interfaces/user-state.interface'
@@ -7,6 +8,7 @@ import {IUserState} from '../../modules/admin/components/user/interfaces/user-st
 export interface IAppState {
   auth: IAuthState
   permissions: IPermissionsState
+  permission: IPermissionState
   roles: IRolesState
   users: IUsersState
   user: IUserState
