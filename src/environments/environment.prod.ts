64d532da7cee469e6666b69945f42633bf5daace
AnViSe <anvise@grodno.belpost.by>
2022-10-18 16:32:46 +0300

    Upd: Prod environment append urlApi

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,7 @@ export const environment = {
   production: true,
   title: 'Бизнес-портал',
   version: '0.0.8',
+  urlApi: '/api/v1',
   urlApiAuth: '/api/v1/auth/',
   urlApiPdp: '/api/v1/sorting/',
 
