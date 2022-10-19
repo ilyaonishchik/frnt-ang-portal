1a38f9c89b0dfe4123bfe1c5ac8ac20b4cee0997
AnViSe <anvise@grodno.belpost.by>
2022-10-19 23:05:03 +0300

    Upd: Delete dialog

--- src/app/shared/modules/table/components/dialog/delete/delete.component.ts
+++ src/app/shared/modules/table/components/dialog/delete/delete.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core'
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core'
 
 @Component({
   selector: 'avs-table-dialog-delete',
@@ -6,7 +6,29 @@ import {Component, OnInit} from '@angular/core'
   styleUrls: ['./delete.component.scss'],
 })
 export class DeleteComponent implements OnInit {
+  @Input('visible') visible: boolean = false
+  @Output('visibleChange') visibleChange: EventEmitter<boolean> =
+    new EventEmitter<boolean>()
+  @Input('itemInfo') itemInfo: string | null | undefined = undefined
+  @Output('clickConfirm') onClickConfirm: EventEmitter<any> =
+    new EventEmitter<any>()
+  @Output('clickCancel') onClickCancel: EventEmitter<any> =
+    new EventEmitter<any>()
+
   constructor() {}
 
   ngOnInit(): void {}
+
+  onVisibleChange(value: boolean): void {
+    this.visible = value
+    this.visibleChange.emit(value)
+  }
+
+  confirmDelete(): void {
+    this.onClickConfirm.emit()
+  }
+
+  cancelDelete(): void {
+    this.onClickCancel.emit()
+  }
 }
