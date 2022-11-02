93443bdc13676d746fee76ac465e542c35f3a48b
AnViSe <anvise@grodno.belpost.by>
2022-11-02 15:22:56 +0300

    Upd: Version 0.0.9

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,13 +5,13 @@
 export const environment = {
   production: false,
   title: 'Бизнес-портал',
-  version: '0.0.8',
+  version: '0.0.9',
   urlApi: '/api/v1',
   urlApiAuth: '/api/v1/auth/',
   urlApiPdp: '/api/v1/sorting/',
 
   rowsPerPageCount: 15,
-  rowsPerPageOptions: [10, 15, 20, 25],
+  rowsPerPageOptions: [5, 10, 15, 20, 25],
 
   adminRoleCode: 'role:administrator',
   adminPermissionCode: 'permission:administrator',
