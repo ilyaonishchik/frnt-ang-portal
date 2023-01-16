24ffcde5122fe260c5fb23e117a2a07cea175d70
AnViSe <anvise@grodno.belpost.by>
2023-01-16 17:17:50 +0300

    Upd: Module Profile

--- src/app/modules/profile/components/main/main.component.ts
+++ src/app/modules/profile/components/main/main.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core'
+import {Store} from '@ngrx/store'
+import {Observable} from 'rxjs'
+import {IUser} from '@shared/interfaces/user.interface'
+import {currentUserSelector} from '@modules/auth/store/selectors'
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
-  styleUrls: ['./main.component.scss']
+  styleUrls: ['./main.component.scss'],
 })
-export class MainComponent {
+export class MainComponent implements OnInit {
+  currentUser$!: Observable<IUser | null>
+  constructor(private store: Store) {}
 
+  ngOnInit(): void {
+    this.currentUser$ = this.store.select(currentUserSelector)
+  }
 }
--- src/app/modules/profile/profile.module.ts
+++ src/app/modules/profile/profile.module.ts
@@ -3,9 +3,11 @@ import {CommonModule} from '@angular/common'
 
 import {ProfileRoutingModule} from './profile-routing.module'
 import {MainComponent} from './components/main/main.component'
+import {AvatarModule} from 'primeng/avatar'
+import {InputTextModule} from 'primeng/inputtext'
 
 @NgModule({
   declarations: [MainComponent],
-  imports: [CommonModule, ProfileRoutingModule],
+  imports: [CommonModule, ProfileRoutingModule, AvatarModule, InputTextModule],
 })
 export class ProfileModule {}
