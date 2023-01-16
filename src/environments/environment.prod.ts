148cb7bb4fa96f0dbe9a44f8642b65699cc1f754
AnViSe <anvise@grodno.belpost.by>
2023-01-16 17:19:12 +0300

    Upd: Components & project version

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   title: 'Бизнес-портал',
-  version: '0.1.1',
+  version: '0.1.2',
   urlApi: '/api/v1',
   urlApiAuth: '/api/v1/auth',
   urlApiPdp: '/api/v1/sorting',
