bd3c4492e1007a7c1f5f8a28d19e9d48e9cc61d2
AnViSe <anvise@grodno.belpost.by>
2023-02-22 14:29:52 +0300

    Upd: Add permissions

--- src/app/modules/admin/admin-routing.module.ts
+++ src/app/modules/admin/admin-routing.module.ts
@@ -3,25 +3,29 @@ import {RouterModule, Routes} from '@angular/router'
 
 import {MainComponent} from './components/main/main.component'
 import {DashboardComponent} from './components/dashboard/dashboard.component'
+import {PermissionGuard} from '../../guards/permission.guard'
 
 const routes: Routes = [
   {path: '', component: MainComponent},
   {path: 'dashboard', component: DashboardComponent},
   {
     path: 'users',
+    canActivate: [PermissionGuard],
+    data: {permission: 'user:read'},
     loadChildren: () =>
       import('./sections/auth/users/users.module').then((m) => m.UsersModule),
   },
   {
     path: 'roles',
+    canActivate: [PermissionGuard],
+    data: {permission: 'role:read'},
     loadChildren: () =>
       import('./sections/auth/roles/roles.module').then((m) => m.RolesModule),
   },
   {
     path: 'permissions',
-    // canLoad: [RoleGuard],
-    // canActivate: [RoleGuard],
-    // data: {role: 'permissions:view'},
+    canActivate: [PermissionGuard],
+    data: {permission: 'permission:read'},
     loadChildren: () =>
       import('./sections/auth/permissions/permissions.module').then(
         (m) => m.PermissionsModule
@@ -29,11 +33,15 @@ const routes: Routes = [
   },
   {
     path: 'links',
+    canActivate: [PermissionGuard],
+    data: {permission: 'link:read'},
     loadChildren: () =>
       import('./sections/portal/links/links.module').then((m) => m.LinksModule),
   },
   {
     path: 'menus',
+    canActivate: [PermissionGuard],
+    data: {permission: 'menu:read'},
     loadChildren: () =>
       import('./sections/portal/menus/menus.module').then((m) => m.MenusModule),
   },
