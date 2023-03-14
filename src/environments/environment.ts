6dad225c4376593ba99154db4d908fea01dc13c6
AnViSe <anvise@grodno.belpost.by>
2023-03-14 16:46:15 +0300

    Upd: Version 0.1.6

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,13 +5,12 @@
 export const environment = {
   production: false,
   title: 'Бизнес-портал',
-  version: '0.1.5',
+  version: '0.1.6',
   urlApi: '/api/v1',
   urlApiAuth: '/api/v1/auth',
   urlApiPdp: '/api/v1/sorting',
   urlApiStorage: '/api/v1/storage',
   urlApiReport: '/api/v1/report',
-  // urlApiReport: 'http://172.16.190.86:8097',
 
   rowsPerPageCount: 15,
   rowsPerPageOptions: [5, 10, 15, 20, 25],
